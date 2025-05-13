a4485c44df578025d3c55649bbd3cc6ced7f7fd4
Luciano Carvalho <lucianoalves413@gmail.com>
2025-05-13 10:18:42 -0300

    Fix: corriguido empresa create e update

--- app/Http/Controllers/EmpresaController.php
+++ app/Http/Controllers/EmpresaController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Services\EmpresaService;
 use App\Services\EnderecoService;
+use Dotenv\Exception\ValidationException;
 use Exception;
 use Illuminate\Support\Facades\Auth;
 
@@ -33,7 +34,6 @@ class EmpresaController extends Controller
             } else {
                 return view('empresa._form', compact('empresas', 'enderecos'));
             }
-
         } catch (Exception $e) {
             dd($e->getMessage());
         }
@@ -60,7 +60,6 @@ class EmpresaController extends Controller
                 return redirect()->route('empresas.index')->with('success', 'Empresa cadastrada com sucesso!');
             }
             return redirect()->back()->withInput()->withErrors('error', 'Error');
-
         } catch (Exception $e) {
             return redirect()->back()->withInput()->withErrors('error', 'Error');
         }
@@ -72,16 +71,27 @@ class EmpresaController extends Controller
             $this->validate($request, [
                 'cnpj' => 'required|string|max:18',
                 'razao_social' => 'required|string|max:255',
-                'endereco_id' => 'required|integer',
+            ], [
+                'cnpj.required' => 'O campo CNPJ é obrigatório.',
+                'cnpj.string' => 'O CNPJ deve ser um texto.',
+                'cnpj.max' => 'O CNPJ não pode ter mais que 18 caracteres.',
+
+                'razao_social.required' => 'O campo Razão Social é obrigatório.',
+                'razao_social.string' => 'A Razão Social deve ser um texto.',
+                'razao_social.max' => 'A Razão Social não pode ter mais que 255 caracteres.',
+
             ]);
+
 
             $empresa = $this->empresaService->atualizarEmpresa($id, $request->all());
             if ($empresa) {
                 return redirect()->route('empresas.index')->with('success', 'Empresa atualizada com sucesso!');
             }
-            return redirect()->back()->withInput()->withErrors('error', 'Error');
+            return redirect()->back()->withInput()->with('error', 'Error');
+        } catch (ValidationException $e) {
+            return redirect()->back()->withInput()->with('error', $e->getMessage());
         } catch (Exception $e) {
-            return redirect()->back()->withInput()->withErrors('error', 'Error');
+            return redirect()->back()->withInput()->with('error', $e->getMessage());
         }
     }
 
